1f3e1a9fb0e351cf022780ef74f5273b7fdf834d
willlee1995 <willlee19951009@gmail.com>
2021-07-18 01:02:00 +0800

    use apply to save state

--- src/Components/FormsUI/ImageMarker/index.js
+++ src/Components/FormsUI/ImageMarker/index.js
@@ -1,26 +1,48 @@
 import ImageMarker from 'react-image-marker'
 import { useState, useEffect } from 'react'
 import Anatomy from '../../../anatomy.svg'
-import { useField, useFormikContext } from 'formik' 
+import { useField, useFormikContext } from 'formik'
+import { Button } from '@material-ui/core';
 
 const MarkerWrapper = ({
     name,
     ...rest
 }) => {
- 
-    const [markers, setMarkers] = useState([])
-    const [field, meta] = useField(name)
+    const initState = []
     const { setFieldValue } = useFormikContext()
+    const [markers, setMarkers] = useState(initState)
+    const [field, meta] = useField(name)
+    const handleApply = () => {
+        setFieldValue(name, markers)
+        
+        console.log(markers)
+    }
+    const handleDelete = () => {
+        setMarkers([])
+    }
+    const handleRemove = () => {
+        var markersArray = [...markers]
+        console.log("ori", markersArray)
+        markersArray.pop()
+        console.log( [markersArray])
+        if (markersArray.length = 0) {
+            setMarkers(initState)
+        } else {
+            console.log("else", markersArray)
+            setMarkers(markersArray)
+        }
+       // Need to research for ways to splice an array without flattening//
+    }
     const HandleAdd = (marker) => {
         setMarkers([...markers, marker])
         console.log(marker)
         console.log(markers)
     }
     useEffect((name) => {
-        setFieldValue(name, markers)
+       
         console.log("effect", markers)
     }, [markers])
-    const configImageMarker =  {
+    const configImageMarker = {
         ...field,
         ...rest,
         src: Anatomy,
@@ -28,16 +50,25 @@ const MarkerWrapper = ({
         onAddMarker: HandleAdd
     }
 
-    if (meta && meta.touched && meta.error){
+    if (meta && meta.touched && meta.error) {
         configImageMarker.error = true
         configImageMarker.helperText = meta.error
     }
     return (
+        <>
         <div>
-            <ImageMarker
-                {...configImageMarker}
-                />
+            <ImageMarker {...configImageMarker} />
         </div>
+        <div>
+            <Button center onClick={handleDelete} variant="contained" color="secondary">Reset</Button>
+        </div>
+        <div>
+            <Button center onClick={handleApply} variant="contained" color="secondary">Apply</Button>
+        </div>
+        <div>
+            <Button center onClick={handleRemove} variant="contained" color="secondary">Delete last marker</Button>
+        </div>
+        </>
     )
 }
 
